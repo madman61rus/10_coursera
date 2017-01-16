9ad760bfbb5bab69380cd70d43ab61cf0ff2c87b
Vladimir Kompaniec <vkompaniec@gmail.com>
2017-01-16 13:41:47 +0300

    -+ Заменил название функции get_courses_list на get_courses_random_list чтобы было понятно что она возвращает рандомный список курсов -+ Переделал заполнение словаря в функции get_course_info -+ Убрал копипасту counter + 2 из функции prepare_output_courses -+ Перенес подготовку данных из output_courses_info_to_xlsx в функцию      prepare_output_courses

--- coursera.py
+++ coursera.py
@@ -7,7 +7,7 @@ from openpyxl import Workbook
 import argparse
 
 
-def get_courses_list(url, number_results):
+def get_courses_random_list(url, number_results):
     courses_list = requests.get(url)
     courses_xml = etree.fromstring(courses_list.content)
     courses_urls = [child[0].text for child in courses_xml]
@@ -15,16 +15,17 @@ def get_courses_list(url, number_results):
 
 
 def get_course_info(url):
-    result_info = {}
-    result_info['course_url'] = url
     try:
         course_html = requests.get(url)
         soup = BeautifulSoup(course_html.content, 'html.parser')
-        result_info['title'] = get_courses_title(soup)
-        result_info['language'] = get_courses_language(soup)
-        result_info['start_date'] = get_courses_start(soup)
-        result_info['weeks'] = get_courses_weeks(soup)
-        result_info['stars'] = get_courses_stars(soup)
+        result_info = {
+            'course_url' : url,
+            'title' : get_courses_title(soup),
+            'language' : get_courses_language(soup),
+            'start_date' : get_courses_start(soup),
+            'weeks' : get_courses_weeks(soup),
+            'stars' : get_courses_stars(soup)
+        }
         return (result_info)
     except:
         return None
@@ -56,8 +57,7 @@ def get_courses_stars(soup):
     stars = soup.find('div', {'class': 'ratings-text bt3-visible-xs'}).contents[0]
     return stars.replace('stars', '')
 
-
-def output_courses_info_to_xlsx(filepath, cources):
+def prepare_output_courses(cources):
     wb = Workbook()
     ws = wb.active
     ws['A1'] = 'Title'
@@ -68,15 +68,19 @@ def output_courses_info_to_xlsx(filepath, cources):
     ws['F1'] = 'Url'
 
     for counter, course_info in enumerate(cources):
+        counter += 2
+        ws.cell(row=counter, column=1, value=course_info['title'])
+        ws.cell(row=counter, column=2, value=course_info['language'])
+        ws.cell(row=counter, column=3, value=course_info['start_date'])
+        ws.cell(row=counter, column=4, value=course_info['weeks'])
+        ws.cell(row=counter, column=5, value=course_info['stars'])
+        ws.cell(row=counter, column=6, value=course_info['course_url'])
 
-            ws.cell(row=counter + 2, column=1, value=course_info['title'])
-            ws.cell(row=counter + 2, column=2, value=course_info['language'])
-            ws.cell(row=counter + 2, column=3, value=course_info['start_date'])
-            ws.cell(row=counter + 2, column=4, value=course_info['weeks'])
-            ws.cell(row=counter + 2, column=5, value=course_info['stars'])
-            ws.cell(row=counter + 2, column=6, value=course_info['course_url'])
+    return wb
 
-    wb.save(filename=filepath)
+def output_courses_info_to_xlsx(filepath, workbook):
+
+    workbook.save(filename=filepath)
 
 
 if __name__ == '__main__':
@@ -86,8 +90,8 @@ if __name__ == '__main__':
     arguments = args.parse_args()
 
     print('Получаем информацию с coursera.org...')
-    courses_urls = get_courses_list('https://www.coursera.org/sitemap~www~courses.xml', int(arguments.results))
+    courses_urls = get_courses_random_list('https://www.coursera.org/sitemap~www~courses.xml', int(arguments.results))
     print('Обрабатываем информацию ...')
     courses_info = [get_course_info(url) for url in courses_urls if get_course_info(url)]
     print('Сохраняем информацию в файл')
-    output_courses_info_to_xlsx(arguments.file, courses_info)
+    output_courses_info_to_xlsx(arguments.file, prepare_output_courses(courses_info))
